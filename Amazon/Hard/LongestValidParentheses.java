d5ba9160c084faa588ee31a8e20d976659824910
Abhi_Raj <abhishubhraj111@gmail.com>
2025-10-08 21:45:53 +0530

    hard +1

--- Amazon/Hard/LongestValidParentheses.java
+++ Amazon/Hard/LongestValidParentheses.java
@@ -1,3 +1,34 @@
+import java.util.*;
+
 public class LongestValidParentheses {
-    
+    public static int longestValidateParentheses(String str){
+        Stack<Integer> st = new Stack<>();
+        st.push(-1);
+        int ans = 0;
+
+        for(int i = 0;i<str.length() ;i++){
+            char ch = str.charAt(i);
+
+            if(ch == '('){
+                st.push(i);
+            }
+            else{
+                if(!st.isEmpty()){
+                    st.pop();
+                }
+                if(!st.isEmpty()){
+                    ans = Math.max(ans, i - st.peek());
+                }else{
+                    st.push(i);
+                }
+            }
+        }
+        return ans;
+    }
+    public static void main(String args[]){
+        String str = "(()";
+        String str1 = ")()())";
+        System.out.println(longestValidateParentheses(str));
+        System.out.println(longestValidateParentheses(str1));
+    }
 }
